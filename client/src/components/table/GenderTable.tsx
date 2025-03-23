51fafaefbe99757e4e792c5cb470b2a9fd46b9a2
Your Name <your-email@example.com>
2025-03-23 15:00:40 +0800

    Add Bootstrap and React Router, implement Genders page with form and table

--- client/src/components/table/GenderTable.tsx
+++ client/src/components/table/GenderTable.tsx
@@ -1,25 +1,56 @@
-const GenderTable = () => {
+const GendersTable = () => {
   return (
     <>
       <table className="table table-hover">
         <thead>
-          <tr>
+          <tr className="align-middle">
             <th>No.</th>
             <th>Gender</th>
+            <th>Action</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
+          <tr className="align-middle">
             <td>1</td>
             <td>Male</td>
+            <td>
+              <div className="btn-group">
+                <button type="button" className="btn btn-info">
+                  Edit
+                </button>
+                <button type="button" className="btn btn-danger">
+                  Delete
+                </button>
+              </div>
+            </td>
           </tr>
-          <tr>
+          <tr className="align-middle">
             <td>2</td>
             <td>Female</td>
+            <td>
+              <div className="btn-group">
+                <button type="button" className="btn btn-info">
+                  Edit
+                </button>
+                <button type="button" className="btn btn-danger">
+                  Delete
+                </button>
+              </div>
+            </td>
           </tr>
-          <tr>
+          <tr className="align-middle">
             <td>3</td>
             <td>Others</td>
+            <td>
+              <div className="btn-group">
+                <button type="button" className="btn btn-info">
+                  Edit
+                </button>
+                <button type="button" className="btn btn-danger">
+                  Delete
+                </button>
+              </div>
+            </td>
           </tr>
         </tbody>
       </table>
@@ -27,4 +58,4 @@ const GenderTable = () => {
   );
 };
 
-export default GenderTable;
+export default GendersTable;
